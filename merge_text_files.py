e280e6d58a1f928401b76298ec52589eb113784b
hooman67 <houman_sh2001@hotmail.com>
2021-11-03 22:48:52 -0700

    add args and don't add header

--- merge_text_files.py
+++ merge_text_files.py
@@ -1,17 +1,34 @@
 import os
+from argparse import ArgumentParser
 
-path2files = "/home/hooman/hs_datasets/fromProduction/car_reid/release1.0.0_2021-10-09_httpApi/try11_reportingInterval100_decision100/text_files/"
-destPath = "/home/hooman/hs_datasets/fromProduction/car_reid/release1.0.0_2021-10-09_httpApi/try11_reportingInterval100_decision100/text_files/merged_results.txt"
 
-out_file = open(destPath,"w")
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input_path', type=str, required=True, help="Path to the prediction file output by the worker")
+    return parser.parse_args()
 
-for file in os.listdir(path2files):
-	in_file = open(path2files + file, "r")
-	lines = in_file.readlines()
-	in_file.close()
+def mergeFiles(path2files):
+	destPath = "/".join(path2files.split("/")[:-2]) + "/merged_results.txt"
 
-	for line in lines:
-		out_file.write(line)
+	out_file = open(destPath,"w")
+	alreadyAddedHeader = False
 
-out_file.close()
+	for file in os.listdir(path2files):
+		in_file = open(path2files + file, "r")
+		lines = in_file.readlines()
+		in_file.close()
+
+		for line in lines:
+			if "cameraId" not in line:
+				out_file.write(line)
+			elif not alreadyAddedHeader:
+				alreadyAddedHeader = True
+				
+
+	out_file.close()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    mergeFiles(args.input_path)
 
