941afb5a2c214298b65975118aa75d10f09e6c13
hooman67 <houman_sh2001@hotmail.com>
2022-01-06 16:29:55 -0800

    add new way of formatting

--- reid_reformat_output.py
+++ reid_reformat_output.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 def get_args():
     parser = ArgumentParser()
     parser.add_argument('-i', '--input_path', type=str, required=True, help="Path to folder containing gallery and result_images folders")
+    parser.add_argument('-t', '--type', type=str, required=True, help="0 for normal 1 for shenzen")
     return parser.parse_args()
 
 def reformat(pathSrc):
@@ -17,7 +18,21 @@ def reformat(pathSrc):
 		for tracklet in os.listdir(resultPath + track):
 			shutil.copy(galPath + tracklet + "/car.jpg", pathDst + track + "_" + tracklet + ".jpg")
 
+def reformat_shenzen(pathSrc):
+	pathDst = pathSrc + "reformatted_results/"
+	resultPath = pathSrc + "result_images/"
+
+	os.mkdir(pathDst)
+
+	for track in os.listdir(resultPath):
+		for tracklet in os.listdir(resultPath + track):
+			shutil.copy(resultPath + track + "/" + tracklet + "/car.jpg", pathDst + track + "_" + tracklet + ".jpg")
 
 if __name__ == '__main__':
     args = get_args()
-    reformat(args.input_path)
+    if args.type == "0":
+    	reformat(args.input_path)
+    elif args.type == "1":
+    	reformat_shenzen(args.input_path)
+    else:
+    	print("\n\n***\nERROR wrong conversion type specified. You passed: ", args.type, "\n***\n\n")
